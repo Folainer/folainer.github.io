b81ba0feebaf75540fd16175c2f3b35182a2cb56
Krutoguz Max <mak.krutogouz@gmail.com>
2022-01-25 16:29:42 +0200

    25.01

---
+++ maket1/script.js
@@ -0,0 +1,25 @@
+let photos = document.querySelectorAll(".photo>div")
+function look1(){
+    photos[0].style.display = "block"
+}
+function look2(){
+    photos[1].style.display = "block"
+}
+function look3(){
+    photos[2].style.display = "block"
+}
+function look4(){
+    photos[3].style.display = "block"
+}
+function un1(){
+    photos[0].style.display = "none"
+}
+function un2(){
+    photos[1].style.display = "none"
+}
+function un3(){
+    photos[2].style.display = "none"
+}
+function un4(){
+    photos[3].style.display = "none"
+}
